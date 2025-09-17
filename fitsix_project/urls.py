81e17698ef4e5685127acde9e45302ab1e7ebec2
sd-powell <kf210rhd@students.codeinstitute.net>
2025-09-17 15:29:50 +0100

    add background image to hero and add static and media links to settings

--- fitsix_project/urls.py
+++ fitsix_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
     # path('bag/', include('bag.urls')),
     # path('checkout/', include('checkout.urls')),
     # path('profile/', include('profiles.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
