778213bf989b181f818ad437f403de7b8bb615b2
sd-powell <kf210rhd@students.codeinstitute.net>
2025-09-17 13:39:19 +0100

    add search bar to navbar

--- fitsix_project/urls.py
+++ fitsix_project/urls.py
@@ -21,4 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    # path('products/', include('products.urls')),
+    # path('profile/', include('profiles.urls')),
+    # path('bag/', include('bag.urls')),
+    # path('checkout/', include('checkout.urls')),
+    # path('profile/', include('profiles.urls')),
 ]
