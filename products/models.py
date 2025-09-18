24002bac0f74bcf9e9333db6fd9587f23676a509
sd-powell <kf210rhd@students.codeinstitute.net>
2025-09-18 13:42:46 +0100

    update products models to allow for product variant model

--- products/models.py
+++ products/models.py
@@ -16,15 +16,29 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
-    size = models.CharField(max_length=50, null=True, blank=True)
-    colour = models.CharField(max_length=50, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    has_variants = models.BooleanField(default=False)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='variants')
+    sku = models.CharField(max_length=254, unique=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    stock = models.PositiveIntegerField(default=0)
+    size = models.CharField(max_length=5, null=True, blank=True)
+    colour = models.CharField(max_length=50, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.size or 'One Size'} - {self.colour or 'Default'}"
