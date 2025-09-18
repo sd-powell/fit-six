61b5f42dbd5a6eeab87d435d98f508a0346364d4
sd-powell <kf210rhd@students.codeinstitute.net>
2025-09-18 10:25:40 +0100

    customised admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+        list_display = (
+        'sku',
+        'name',
+        'category',
+        'size',
+        'colour',
+        'price',
+        'image',
+    )
+        
+        ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+        list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
