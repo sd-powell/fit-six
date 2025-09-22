5cf6da98430550976554cf120140f8f3972147f2
sd-powell <kf210rhd@students.codeinstitute.net>
2025-09-22 17:01:10 +0100

    add checkout views and templates

--- fitsix_project/urls.py
+++ fitsix_project/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('products/', include('products.urls')),
     # path('profile/', include('profiles.urls')),
     path('bag/', include('bag.urls')),
-    # path('checkout/', include('checkout.urls')),
+    path('checkout/', include('checkout.urls')),
     # path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
