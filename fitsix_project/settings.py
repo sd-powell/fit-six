2cc3753bb71bb7378618827629e9b2c0f52242d0
sd-powell <kf210rhd@students.codeinstitute.net>
2025-09-22 17:01:10 +0100

    add checkout views and templates

--- fitsix_project/settings.py
+++ fitsix_project/settings.py
@@ -47,6 +47,10 @@ INSTALLED_APPS = [
     'bag',
     'checkout.apps.CheckoutConfig',
     'django_countries',
+    
+    # other
+    "crispy_forms",
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +66,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'fitsix_project.urls'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -75,7 +82,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
