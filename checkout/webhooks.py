4c1d8f4ab98e758c10c95352be9a33d4a3b54195
sd-powell <kf210rhd@students.codeinstitute.net>
2025-09-23 09:51:25 +0100

    add webhook handler to handle product variants

---
+++ checkout/webhooks.py
@@ -0,0 +1,57 @@
+from django.http import HttpResponse
+from django.conf import settings
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from checkout.webhook_handler import StripeWH_Handler
+
+import stripe
+
+
+@require_POST
+@csrf_exempt
+def webhook(request):
+    """Listen for webhooks from Stripe"""
+    # Setup
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    # Get the webhook data and verify its signature
+    payload = request.body
+    sig_header = request.META.get('HTTP_STRIPE_SIGNATURE')  # Use .get() for safety
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, wh_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(content="Invalid payload", status=400)
+
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(content="Invalid signature", status=400)
+
+    except Exception as e:
+        return HttpResponse(content=f"Error: {str(e)}", status=400)
+
+    # Set up a webhook handler
+    handler = StripeWH_Handler(request)
+
+    # Map webhook events to relevant handler functions
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
+
+    # Get the webhook type from Stripe
+    event_type = event['type']
+
+    # Get the appropriate handler
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    # Call the handler
+    print('Success!')
+    response = event_handler(event)
+    return response
