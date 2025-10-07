6b4b4ec73fdf5582a5c23633aa8537ecc371582c
sd-powell <kf210rhd@students.codeinstitute.net>
2025-10-07 20:31:34 +0100

    add image_back model for variant image support

--- products/admin.py
+++ products/admin.py
@@ -2,24 +2,47 @@ from django.utils.html import format_html
 from django.contrib import admin
 from .models import Category, Product, ProductVariant
 
+
 class CategoryAdmin(admin.ModelAdmin):
-        list_display = ('friendly_name', 'name',)
+    list_display = ('friendly_name', 'name',)
 
 
-class ProductVariantInline(admin.TabularInline):  # Or use StackedInline if you prefer
+class ProductVariantInline(admin.TabularInline):
     model = ProductVariant
-    extra = 1  # Number of blank variant forms shown by default
+    extra = 1
     show_change_link = True
     verbose_name_plural = "Variants"
-    readonly_fields = ['preview']
-    fields = ('sku', 'price', 'stock', 'size', 'colour', 'image', 'preview')
-    
+    readonly_fields = ['preview', 'preview_back']
+    fields = (
+        'sku',
+        'price',
+        'stock',
+        'size',
+        'colour',
+        'image',
+        'preview',
+        'image_back',
+        'preview_back'
+        )
+
     def preview(self, obj):
         if obj.image:
-            return format_html('<img src="{}" style="height: 60px;" />', obj.image.url)
+            return format_html(
+                '<img src="{}" style="height: 60px;" />',
+                obj.image.url
+                )
         return "(No image)"
 
-    preview.short_description = 'Image Preview'
+    def preview_back(self, obj):
+        if obj.image_back:
+            return format_html(
+                '<img src="{}" style="height: 60px;" />',
+                obj.image_back.url
+                )
+        return "(No back image)"
+
+    preview.short_description = 'Front Preview'
+    preview_back.short_description = 'Back Preview'
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -27,26 +50,57 @@ class ProductAdmin(admin.ModelAdmin):
     ordering = ('name',)
     inlines = [ProductVariantInline]
     readonly_fields = ['image_preview']
-    fields = ('name', 'category', 'has_variants', 'description', 'image', 'image_preview')
+    fields = (
+        'name',
+        'category',
+        'has_variants',
+        'description',
+        'image',
+        'image_preview'
+        )
 
     def image_preview(self, obj):
         if obj.image:
-            return format_html('<img src="{}" style="height: 100px;" />', obj.image.url)
+            return format_html(
+                '<img src="{}" style="height: 100px;" />',
+                obj.image.url
+                )
         return "(No image)"
 
     image_preview.short_description = 'Image Preview'
 
 
 class ProductVariantAdmin(admin.ModelAdmin):
-    list_display = ('sku', 'product', 'price', 'stock', 'size', 'colour', 'variant_image_preview')
-    readonly_fields = ['variant_image_preview']
+    list_display = (
+        'sku',
+        'product',
+        'price',
+        'stock',
+        'size',
+        'colour',
+        'variant_image_preview',
+        'variant_back_image_preview'
+        )
+    readonly_fields = ['variant_image_preview', 'variant_back_image_preview']
 
     def variant_image_preview(self, obj):
         if obj.image:
-            return format_html('<img src="{}" style="height: 60px;" />', obj.image.url)
+            return format_html(
+                '<img src="{}" style="height: 60px;" />',
+                obj.image.url
+                )
         return "(No image)"
 
-    variant_image_preview.short_description = 'Image Preview'
+    def variant_back_image_preview(self, obj):
+        if obj.image_back:
+            return format_html(
+                '<img src="{}" style="height: 60px;" />',
+                obj.image_back.url
+                )
+        return "(No back image)"
+
+    variant_image_preview.short_description = 'Front Image'
+    variant_back_image_preview.short_description = 'Back Image'
 
 
 admin.site.register(Product, ProductAdmin)
