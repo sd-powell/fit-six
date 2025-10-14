5c3a9773b99a6d0b4b165d4db572cae8bdc80b72
sd-powell <kf210rhd@students.codeinstitute.net>
2025-10-14 22:16:11 +0100

    add custom 404 error page and url redirect

--- fitsix_project/urls.py
+++ fitsix_project/urls.py
@@ -28,4 +28,10 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('newsletter/', include('newsletter.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+handler404 = 'home.views.custom_404'
--- home/views.py
+++ home/views.py
@@ -5,3 +5,12 @@ def index(request):
     """ A view to return the index page """
     
     return render(request, 'home/index.html')
+
+def custom_404(request, exception):
+    """
+    Custom 404 error handler.
+
+    Renders a friendly 404 page when a page is not found.
+    Django automatically passes the exception as a second argument.
+    """
+    return render(request, '404.html', status=404)
