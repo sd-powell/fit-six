3083a4041ef446d85ea3fb9d108cc486988c661f
sd-powell <kf210rhd@students.codeinstitute.net>
2025-10-18 00:35:26 +0100

    Add AWS_DEFAULT_ACL and confirm USE_AWS

--- fitsix_project/settings.py
+++ fitsix_project/settings.py
@@ -181,7 +181,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if 'USE_AWS' in os.environ:
+if os.environ.get('USE_AWS') == 'True':
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
@@ -194,6 +194,7 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_DEFAULT_ACL = None
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
